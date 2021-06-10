55d4c90a269fa3efb44383ae2665b5fd5f6f69b4
Paul Schultz <tanksrule1015@gmail.com>
2021-06-10 16:51:50 -0500

    Fixed logo sizing and changed devfile registry button color

--- components/page/Layout.tsx
+++ components/page/Layout.tsx
@@ -1,7 +1,7 @@
 import Meta from '@components/page/Meta'
 
 import Link from 'next/link'
-import { Brand, Page, PageHeader, PageHeaderTools, PageSection, PageSectionVariants } from '@patternfly/react-core'
+import { Text, TextContent, Brand, Page, PageHeader, PageHeaderTools, PageSection, PageSectionVariants } from '@patternfly/react-core'
 
 import mainPageLogo from '../../public/mainPageLogo.svg'
 
@@ -10,22 +10,19 @@ interface Props {
 }
 
 const Layout = ({ children }: Props): React.ReactElement => {
-  const logoProps = {
-    href: '',
-    onClick: () => console.log('clicked logo'),
-    target: '_blank'
-  }
-
   const Header = (
     <PageHeader
       logo={
         <Brand src={mainPageLogo} alt="Devfile Registry Logo" className="h-full"/>
       }
-      logoProps={logoProps}
       headerTools={
         <PageHeaderTools>
           <Link href='/'>
-            <a>Devfile Registry</a>
+            <a>
+              <TextContent>
+                <Text>Devfile Registry</Text>
+              </TextContent>
+            </a>
           </Link>
         </PageHeaderTools>
       }
