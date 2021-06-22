1c9fcf0c52102471db1f0e395f9c5f6e163d66ff
Paul Schultz <tanksrule1015@gmail.com>
2021-06-22 13:57:35 -0500

    Updated packages

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const withTM = require('next-transpile-modules')([
 ])
 
 module.exports = withPlugins([withTM, withImages, withPWA], {
+  images: {
+    disableStaticImages: true
+  },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     register: true,
