29d5c2a80c2a39188a845b1d646f7d0a6d8cccee
Nadia <nwaid@mit.edu>
2021-06-29 14:21:35 -0700

    added pr suggestions from paul and maysun and completed subdirectory download

--- components/page/DevPageProjects.tsx
+++ components/page/DevPageProjects.tsx
@@ -1,4 +1,6 @@
 import styles from '@components/page/devpage.module.css'
+import JSZip from 'jszip'
+import { saveAs } from 'file-saver'
 
 import {
     Card,
@@ -6,17 +8,13 @@ import {
     CardTitle,
     CardBody,
     CardExpandableContent,
-    Button,
+    Button
   } from '@patternfly/react-core'
 import { useState } from 'react'
 
 /*
     component for starter projects dropdown 
 */
-
-//TODO: subdirectories
-//TODO: fix scroll bar
-//TODO: Show less Tags
 
 /**
  * Representation of a Git source for  a starter project
@@ -67,6 +65,24 @@ interface Props {
     starterProjects: Project[]
 }
 
+async function downloadSubdirectory(url:string, subDir:string){
+    const response = await fetch('/api/download_project',{
+
+        method:"POST",
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify({url:url, subdirectory:subDir})
+    })
+    const base64string = await response.text()
+    let zip = await JSZip.loadAsync(base64string, {base64:true})
+
+    await zip.generateAsync({type:"blob"}).then(function (blob) { 
+        saveAs(blob, subDir+ ".zip");                          
+    }, function (err) {
+        jQuery("#blob").text(err);
+    });
+
+}
+
 /**
  * extract zip url from project git source
  * 
@@ -86,9 +102,13 @@ function getURLForGit(git:Git){
         url=git.remotes[git.checkoutFrom.remote]
 
     } else {
-        throw  TypeError("Invalid git remotes")
+        throw new TypeError("Invalid git remotes")
     }
-    url = url.slice(0,url.length-4).replace("github.com","api.github.com/repos") +"/zipball/" //remove ".git" from link and convert to api zip link
+
+    if(url.includes(".git"))
+        url = url.slice(0,url.length-4)
+
+    url = url.replace("github.com","api.github.com/repos") +"/zipball/" //remove ".git" from link and convert to api zip link
 
     if(git.checkoutFrom&&git.checkoutFrom.revision){
         url += git.checkoutFrom.revision
@@ -103,16 +123,22 @@ function getURLForGit(git:Git){
  * @throws TypeError
  *      thrown if no url locations are found
  */
-function download(project:Project){ 
-
+async function download(project:Project){ 
+    let url:string
     if(project.git){ //for git
-        window.open(getURLForGit(project.git)) 
+        url = getURLForGit(project.git)
 
     } else if (project.zip&& project.zip.location){ //for zip
-        window.open(project.zip.location)
+        url = project.zip.location
 
     } else { 
-        throw  TypeError("Invalid project has no zip url")
+        throw  new TypeError("Invalid project has no zip/git url")
+    }
+
+    if(project.subDir){
+        await downloadSubdirectory(url, project.subDir)
+    } else{
+        window.open(url)
     }
     
 }
@@ -128,13 +154,21 @@ function download(project:Project){
  * @param starterProjects - starter projects associated with respective devfile
  */
 
-const DevfilePageProjects= ({starterProjects}:Props) => {
+const DevPageProjects= ({starterProjects}:Props) => {
 
     const [expanded, setExpanded] = useState<boolean>(false)
+    const [downloading, setDownloading] = useState<boolean>(false)
 
     const [selectedProject, setSelectedProject] = useState<Project>(starterProjects[0])
-    const [viewProject, setViewProject] = useState<Project|null>(null) //hovered project, null when not hovering over project list
+    const [currentlyHoveredProject, setCurrentlyHoveredProject] = useState<Project|null>(null) //null when not hovering over project list
 
+
+    async function triggerDownload(project:Project){
+        setDownloading(true)
+        await download(project)
+        setDownloading(false)
+    }
+    
   return (
       <Card id="card1" 
         isExpanded={expanded}
@@ -156,32 +190,32 @@ const DevfilePageProjects= ({starterProjects}:Props) => {
             <CardBody>
                 <div style={{display:"flex", width:"100%"}}>
 
-                    <div style = {{width:"50%", alignItems:"center", height:"10rem", overflowY:"scroll", order:0 }}
-                        onMouseLeave ={() => setViewProject(null)}>
-                        {starterProjects.map((item) => //starter projects list
+                    <div className= {styles.select} style = {{width:"50%", alignItems:"center", height:"10rem", overflowY:"scroll", order:0 }}
+                        onMouseLeave ={() => setCurrentlyHoveredProject(null)}>
+                        {starterProjects.map((project) => //starter projects list
                         (<div 
-                            id ={item.name}
+                            id ={project.name}
                             style={{width:"95%"}}
-                            onMouseDown={() => (setSelectedProject(item))}
-                            onMouseEnter ={() => setViewProject(item)}
-                            className={(selectedProject.name===item.name)?styles.selected:styles.project}>
-                            {item.name}
+                            onMouseDown={() => (setSelectedProject(project))}
+                            onMouseEnter ={() => setCurrentlyHoveredProject(project)}
+                            className={(selectedProject.name===project.name)?styles.selected:styles.project}>
+                            {project.name}
                         </div>))
                     }
                     </div>
 
                     <div style = {{width:"40%", alignItems:"center",marginLeft:"auto", marginRight:"auto", order:0}}>
-                        {!viewProject? //sets displayed project description
+                        {!currentlyHoveredProject? //sets displayed project description
                             (<div style = {{width:"80%",  padding:"auto", margin:"10px"}}>
                                 <h6>{selectedProject.name}</h6>
                                 <p style={{color:"#ADABAE"}}>{selectedProject.description}</p>
                             </div>):
                             (<div style = {{width:"80%",  padding:"auto", margin:"10px"}}>
-                                <h6>{viewProject.name}</h6>
-                                <p style={{color:"#ADABAE"}}>{viewProject.description}</p>
+                                <h6>{currentlyHoveredProject.name}</h6>
+                                <p style={{color:"#ADABAE"}}>{currentlyHoveredProject.description}</p>
                             </div>)}
                             <div style={{display:"flex", alignContent:"center"}}>
-                                <Button onClick={e => download(selectedProject)}variant="tertiary">download</Button>
+                                <Button className={styles.button} isLoading={downloading} onClick={e => triggerDownload(selectedProject)} variant="tertiary" >download</Button>
                             </div>
                             
                     </div>
@@ -191,4 +225,4 @@ const DevfilePageProjects= ({starterProjects}:Props) => {
         </Card>
   )
 }
-export default DevfilePageProjects
+export default DevPageProjects
