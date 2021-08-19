c15f6f948381878cd5c60ae1bd1be6cf81117261
Paul Schultz <pschultz@pobox.com>
2021-08-19 17:15:09 -0500

    Changed header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,16 @@
 import styles from './Layout.module.css';
 import { Meta } from '@src/components';
-import devfileLogo from '@public/images/devfileLogoWhiteText.svg';
-import Link from 'next/link';
+import devfileLogo from '@public/images/devfileLogo.svg';
 import {
+  Nav,
+  NavItem,
+  NavList,
   Text,
   TextContent,
+  TextVariants,
   Brand,
   Page,
   PageHeader,
-  PageHeaderTools,
   PageSection,
   PageSectionVariants
 } from '@patternfly/react-core';
@@ -16,6 +18,12 @@ import {
 export interface LayoutProps {
   children: React.ReactNode;
 }
+
+// Local refers to a link that is on the same url as the parent
+const navLinks = [
+  { name: 'Getting Started', link: 'https://docs.devfile.io/landing-page/starting', local: false },
+  { name: 'Docs', link: 'https://docs.devfile.io', local: false }
+];
 
 /**
  * Renders a {@link Layout} React component.
@@ -27,23 +35,48 @@ export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
     href: '/'
   };
 
-  const Header = (
-    <PageHeader
-      logo={<Brand src={devfileLogo} alt="Devfile Registry Logo" className={styles.logo} />}
-      logoProps={logoProps}
-      headerTools={
-        <PageHeaderTools>
-          <Link href="/">
-            <a data-testid="go-home-button">
+  const nav = (
+    <div style={{ display: 'flex', justifyContent: 'space-between', flex: '1 1 0%' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Brand src={devfileLogo} alt="Devfile Registry Logo" className={styles.logo} />
+        <TextContent>
+          <Text component={TextVariants.h3}>Devfile Registry</Text>
+        </TextContent>
+      </div>
+      <Nav variant="horizontal">
+        <NavList>
+          {navLinks.map((navLink) =>
+            navLink.local ? (
+              <NavItem key={navLink.name}>
+                <a href={navLink.link}>
+                  <TextContent>
+                    <Text style={{ textDecoration: 'none' }}>{navLink.name}</Text>
+                  </TextContent>
+                </a>
+              </NavItem>
+            ) : (
+              <NavItem key={navLink.name}>
+                <a target="_blank" rel="noreferrer" href={navLink.link}>
+                  <TextContent>
+                    <Text style={{ textDecoration: 'none' }}>{navLink.name}</Text>
+                  </TextContent>
+                </a>
+              </NavItem>
+            )
+          )}
+          <NavItem>
+            <a data-test-id="go-home-button" href="/">
               <TextContent>
-                <Text>Devfile Registry</Text>
+                <Text style={{ textDecoration: 'none' }}>Devfile Registry</Text>
               </TextContent>
             </a>
-          </Link>
-        </PageHeaderTools>
-      }
-    />
+          </NavItem>
+        </NavList>
+      </Nav>
+    </div>
   );
+
+  const Header = <PageHeader logoProps={logoProps} topNav={nav} />;
 
   return (
     <Page header={Header}>
